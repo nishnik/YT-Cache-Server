013d0c9385f24abfb9613585e99d8175e63f3fcc
Nishant Nikhil <nishantiam@gmail.com>
2017-08-16 23:19:25 +0530

    initial working version

--- server.py
+++ server.py
@@ -5,19 +5,12 @@ import argparse
 
 parser = reqparse.RequestParser()
 
-saved_vids = ('ycYewhiaVBk', '0UHwkfhwjsk')
+saved_vids = ('ycYewhiaVBk', '0UHwkfhwjsk', 'kcpUO8K_rek')
 app = Flask(__name__)
 api = Api(app)
 cors = CORS(app)
 
-
-
-# from OpenSSL import SSL
 context = ('cert.pem', 'key.pem')
-# context.use_privatekey_file('key.pem')
-# context.use_certificate_file('cert.pem')
-
-
 
 @app.route('/query', methods=['POST'])
 class Model(Resource):
@@ -42,11 +35,10 @@ def raiseError(error):
 if __name__ == '__main__':
     #----------- Parsing Arguments ---------------
     p = argparse.ArgumentParser()
-    p.add_argument("--host", help="Host name (default: localhost)")
+    p.add_argument("--host", help="Host name (default: 0.0.0.0)")
     p.add_argument("--port", help="Port (default: 5000)")
     args = p.parse_args()
-
-    host = args.host if args.host else "localhost"
+    host = args.host if args.host else "0.0.0.0"
     port = int(args.port) if args.port else 5000
     api.add_resource(Model, '/query')
     app.run(host=host, port=port, ssl_context=context)
